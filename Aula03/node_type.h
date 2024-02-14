8a61446b7a9a8a1b96c3ad2f42045412200677c0
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2024-02-14 05:14:34 -0300

    Aula03-palindromo

--- Aula03/node_type.h
+++ Aula03/node_type.h
@@ -1,12 +1,13 @@
+
+#ifndef NODETYPE_H   // Inclua esse bloco apenas se TIME_H não está definido
+#define NODETYPE_H   // Na primeira inclusão, defina TIME_H para que este
+		 // bloco não seja incluído mais de uma vez.
 typedef char ItemType;
-/*
- Estrutura usada para guardar 
- a informação e o endereço do 
- próximo elemento.
-*/
+
 struct NodeType
 {
   ItemType info;
   NodeType* next;
 };
 
+#endif
